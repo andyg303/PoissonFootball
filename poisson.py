3e416bd2fbfad0f8711a9dfa7a021245be016e8f
Andy Gray <andyg303@gmail.com>
2023-12-10 22:48:13 +0000

    v.0.0.1 - First commit of football modelling stuff

---
+++ poisson.py
@@ -0,0 +1,77 @@
+import pandas as pd
+from scipy.stats import poisson
+
+# Load your data
+data = pd.read_csv('premier-league.csv')
+
+# Calculate league average goals scored and conceded
+league_avg_goals_scored = (data['Criteria1Val'].sum() + data['Criteria2Val'].sum()) / (2 * len(data))
+league_avg_goals_conceded = (data['Criteria6Val'].sum() + data['Criteria7Val'].sum()) / (2 * len(data))
+
+# Function to calculate match probabilities using Poisson distribution
+def calculate_probabilities(home_goals_scored, away_goals_scored, home_goals_conceded, away_goals_conceded):
+    # Adjusted expectancy calculations
+    home_goals_expectancy = home_goals_scored * away_goals_conceded / league_avg_goals_conceded
+    away_goals_expectancy = away_goals_scored * home_goals_conceded / league_avg_goals_conceded
+
+    max_goals = 10
+    home_probs = [poisson.pmf(i, home_goals_expectancy) for i in range(0, max_goals+1)]
+    away_probs = [poisson.pmf(i, away_goals_expectancy) for i in range(0, max_goals+1)]
+
+    home_win_prob = sum(home_probs[i] * sum(away_probs[:i]) for i in range(1, max_goals+1))
+    away_win_prob = sum(away_probs[i] * sum(home_probs[:i]) for i in range(1, max_goals+1))
+    draw_prob = sum(home_probs[i] * away_probs[i] for i in range(max_goals+1))
+
+    over_under_probs = {}
+    for threshold in range(6):
+        over_prob = sum(home_probs[i] * sum(away_probs[j] for j in range(threshold+1, max_goals+1)) for i in range(max_goals+1))
+        over_under_probs[f"Over {threshold}.5"] = over_prob
+        over_under_probs[f"Under {threshold}.5"] = 1 - over_prob
+
+    return home_win_prob, away_win_prob, draw_prob, over_under_probs
+
+# Convert probability to decimal odds
+def prob_to_odds(prob):
+    return round(1 / prob, 2) if prob > 0 else None
+
+# Check if BF odds offer value
+def is_value_bet(bf_odds, calc_odds):
+    return "TRUE" if bf_odds > calc_odds else ""
+
+# Prepare the data for CSV
+output_data = []
+for index, row in data.iterrows():
+    home_goals_expectancy = row['Criteria1Val'] * league_avg_goals_scored
+    away_goals_expectancy = row['Criteria2Val'] * league_avg_goals_scored
+    home_goals_conceded = row['Criteria6Val']
+    away_goals_conceded = row['Criteria7Val']
+
+    home_win_prob, away_win_prob, draw_prob, over_under_probs = calculate_probabilities(home_goals_expectancy, away_goals_expectancy, home_goals_conceded, away_goals_conceded)
+
+    calc_home_win_odds = prob_to_odds(home_win_prob)
+    calc_draw_odds = prob_to_odds(draw_prob)
+    calc_away_win_odds = prob_to_odds(away_win_prob)
+
+    match_data = {
+        'Match': f"{row['Home']} vs {row['Away']}",
+        'Calculated Home Win Odds': calc_home_win_odds,
+        'BFHomeOdds': row['Criteria3Val'],
+        'IsHomeValue': is_value_bet(row['Criteria3Val'], calc_home_win_odds),
+        'Calculated Draw Odds': calc_draw_odds,
+        'BFDrawOdds': row['Criteria4Val'],
+        'IsDrawValue': is_value_bet(row['Criteria4Val'], calc_draw_odds),
+        'Calculated Away Win Odds': calc_away_win_odds,
+        'BFAwayOdds': row['Criteria5Val'],
+        'IsAwayValue': is_value_bet(row['Criteria5Val'], calc_away_win_odds)
+    }
+    
+    for key, value in over_under_probs.items():
+        match_data[key + ' Odds'] = prob_to_odds(value)
+
+    output_data.append(match_data)
+
+# Create a DataFrame and save to CSV
+output_df = pd.DataFrame(output_data)
+output_df.to_csv('match_odds_predictions_refactored.csv', index=False)
+
+print("Refactored odds predictions saved to match_odds_predictions_refactored.csv")
---
+++ xg-scrape.py
@@ -0,0 +1,377 @@
+import requests
+import re
+import json
+import time
+from pymongo import MongoClient
+from scipy.stats import poisson
+
+# URLs array
+urls = [
+    "https://understat.com/league/EPL/2023",
+    # "https://understat.com/league/EPL/2022",
+    # "https://understat.com/league/La_liga/2023"
+]
+
+# Different model ideas
+# 1. Home Goals at Home vs Away Goals Away
+# 2. Overall Goals
+# 3. xG Home at Home vs xG Away Away
+# 4. xG Overall
+# Iterate the above on the following number of games:
+# A) Last 4 Home / Away 
+# B) Last 8 Overall
+# C) This Season
+# D) This Season and Last Season (If avail)
+
+# Finally do various combinations of the above 16 possible variations
+
+# MongoDB connection
+client = MongoClient('localhost', 27017)
+db = client['football_database']
+team_collection = db['team_data']
+match_collection = db['match_data']
+
+# Function to aggregate records
+def aggregate_records(team):
+    overall_record = {
+        'xG': 0, 'xGA': 0, 'scored': 0, 'missed': 0,
+        'wins': 0, 'loses': 0, 'draws': 0, 'matches_played': 0
+    }
+    home_record = overall_record.copy()
+    away_record = overall_record.copy()
+
+    for match in team.get('history', []):
+        overall_record['matches_played'] += 1
+        for key in overall_record:
+            if key in match and key != 'matches_played':
+                overall_record[key] += match[key]
+
+        if match['h_a'] == 'h':
+            home_record['matches_played'] += 1
+            for key in home_record:
+                if key in match and key != 'matches_played':
+                    home_record[key] += match[key]
+        elif match['h_a'] == 'a':
+            away_record['matches_played'] += 1
+            for key in away_record:
+                if key in match and key != 'matches_played':
+                    away_record[key] += match[key]
+
+    # Adding average calculations
+    if overall_record['matches_played'] > 0:
+        overall_record['avxG'] = overall_record['xG'] / overall_record['matches_played']
+        overall_record['avxG_conceded'] = overall_record['xGA'] / overall_record['matches_played']
+        overall_record['avG'] = overall_record['scored'] / overall_record['matches_played']
+        overall_record['avG_conceded'] = overall_record['missed'] / overall_record['matches_played']
+    if home_record['matches_played'] > 0:
+        home_record['avxG'] = home_record['xG'] / home_record['matches_played']
+        home_record['avxG_conceded'] = home_record['xGA'] / home_record['matches_played']
+        home_record['avG'] = home_record['scored'] / home_record['matches_played']
+        home_record['avG_conceded'] = home_record['missed'] / home_record['matches_played']
+    if away_record['matches_played'] > 0:
+        away_record['avxG'] = away_record['xG'] / away_record['matches_played']
+        away_record['avxG_conceded'] = away_record['xGA'] / away_record['matches_played']
+        away_record['avG'] = away_record['scored'] / away_record['matches_played']
+        away_record['avG_conceded'] = away_record['missed'] / away_record['matches_played']
+
+    return overall_record, home_record, away_record
+
+# Function to extract league name from URL
+def extract_league_name(url):
+    # Extracting league name using regular expression
+    match = re.search(r'/league/([^/]+)/', url)
+    if match:
+        return match.group(1)
+    return "Unknown"
+
+# Function to process each URL
+def process_url(url):
+    league_name = extract_league_name(url)
+    response = requests.get(url)
+    if response.status_code == 200:
+
+        # Scrape MATCH data
+        match_counter = 0
+        matches = re.search(r'var datesData\s*=\s*JSON.parse\(\'(.*?)\'\)', response.text)
+        if matches:
+            json_data = matches.group(1).encode().decode('unicode_escape')
+            match_data = json.loads(json_data)
+            match_counter = len(match_data)
+
+            for item in match_data:
+                item['league'] = league_name
+                match_collection.insert_one(item)
+
+        print(f"{match_counter} MATCH records scraped for {league_name}")
+
+        # Scrape TEAM data
+        team_counter = 0
+        matches = re.search(r'var teamsData\s*=\s*JSON.parse\(\'(.*?)\'\)', response.text)
+        if matches:
+            json_data = matches.group(1).encode().decode('unicode_escape')
+            teams_data = json.loads(json_data)
+            team_counter = len(teams_data)
+
+            for team_id, team_info in teams_data.items():
+                team_info['league'] = league_name
+                existing_team = team_collection.find_one({'id': team_id, 'league': league_name})
+                if existing_team:
+                    existing_team['history'].extend(team_info['history'])
+                    team_collection.update_one({'id': team_id, 'league': league_name}, {'$set': {'history': existing_team['history']}})
+                else:
+                    team_info['id'] = team_id
+                    team_collection.insert_one(team_info)
+
+        print(f"{team_counter} TEAM records scraped for {league_name}")
+
+    else:
+        print(f"Failed to retrieve data from {url}")
+
+# Process each URL with a delay
+for url in urls:
+    process_url(url)
+    time.sleep(10)  # 10-second delay between each request
+
+# Update overall, home, and away records for each team
+for team in team_collection.find():
+    overall_record, home_record, away_record = aggregate_records(team)
+    team_collection.update_one(
+        {'_id': team['_id']},
+        {'$set': {
+            'overall_record': overall_record,
+            'home_record': home_record,
+            'away_record': away_record
+        }}
+    )
+print("All team records updated with total matches played.")
+
+# Function to calculate Poisson probability
+def poisson_probability(lmbda, k):
+    return poisson.pmf(k, lmbda)
+
+def calculate_outcomes(avg_home_goals, avg_away_goals):
+    max_goals = 6
+    outcomes = {
+        'home_win': 0,
+        'away_win': 0,
+        'draw': 0,
+        'over_goals': {i: 0 for i in range(7)},
+        'under_goals': {i: 0 for i in range(7)}
+    }
+
+    for home_goals in range(max_goals + 1):
+        for away_goals in range(max_goals + 1):
+            home_prob = poisson_probability(avg_home_goals, home_goals)
+            away_prob = poisson_probability(avg_away_goals, away_goals)
+            match_prob = home_prob * away_prob
+
+            if home_goals > away_goals:
+                outcomes['home_win'] += match_prob
+            elif home_goals < away_goals:
+                outcomes['away_win'] += match_prob
+            else:
+                outcomes['draw'] += match_prob
+
+            total_goals = home_goals + away_goals
+            for i in range(7):
+                if total_goals > i:
+                    outcomes['over_goals'][i] += match_prob
+                else:
+                    outcomes['under_goals'][i] += match_prob
+
+    # Separating the odds calculation for simple outcomes and over/under goals
+    odds = {key: 1 / outcomes[key] if outcomes[key] > 0 else None for key in ['home_win', 'away_win', 'draw']}
+    for i in range(7):
+        odds[f'over_{i}.5'] = 1 / outcomes['over_goals'][i] if outcomes['over_goals'][i] > 0 else None
+        odds[f'under_{i}.5'] = 1 / outcomes['under_goals'][i] if outcomes['under_goals'][i] > 0 else None
+
+    return odds
+
+# Function to get team data by ID
+def get_team_data(team_id):
+    team = team_collection.find_one({'id': team_id})
+    if team:
+        return team['overall_record']  # Or use 'home_record'/'away_record' if relevant
+    return None
+
+# Function to calculate average goals in each league
+def calculate_league_averages():
+    league_averages = {}
+    for team in team_collection.find():
+        league = team.get('league')
+        if not league:
+            print(f"{league} League Error 1")
+            continue  # Skip if league is not specified
+
+        if 'home_record' not in team or 'away_record' not in team or 'overall_record' not in team:
+            print(f"{league} League Error 2")
+            continue  # Skip if required records are missing
+
+        if league not in league_averages:
+            league_averages[league] = {'total_goals': 0, 'total_home_goals': 0, 'total_away_goals': 0, 'total_matches': 0, 'total_home_matches': 0, 'total_away_matches': 0}
+
+        league_averages[league]['total_goals'] += team['overall_record'].get('scored', 0)
+        league_averages[league]['total_matches'] += team['overall_record'].get('matches_played', 0)
+        league_averages[league]['total_home_goals'] += team['home_record'].get('scored', 0)
+        league_averages[league]['total_home_matches'] += team['home_record'].get('matches_played', 0)
+        league_averages[league]['total_away_goals'] += team['away_record'].get('scored', 0)
+        league_averages[league]['total_away_matches'] += team['away_record'].get('matches_played', 0)
+
+    for league, data in league_averages.items():
+        total_matches = data['total_matches']
+        total_home_matches = data['total_home_matches']
+        total_away_matches = data['total_away_matches']
+        data['avg_goals'] = data['total_goals'] / total_matches if total_matches > 0 else 0
+        data['avg_home_goals'] = data['total_home_goals'] / total_home_matches if total_home_matches > 0 else 0
+        data['avg_away_goals'] = data['total_away_goals'] / total_away_matches if total_away_matches > 0 else 0
+
+    return league_averages
+
+time.sleep(5)  # Make sure only runs 10 seconds after the last scrape
+
+# Calculate league averages
+league_averages = calculate_league_averages()
+
+# Process each match to calculate odds
+for match in match_collection.find():
+    league = match.get('league')
+    if not league or league not in league_averages:
+        continue  # Skip if the league data is not available or not specified
+
+    home_team_id = match['h']['id']
+    away_team_id = match['a']['id']
+
+    home_team_data = get_team_data(home_team_id)
+    away_team_data = get_team_data(away_team_id)
+
+    if home_team_data and away_team_data:
+        avg_goals_league = league_averages[league]['avg_goals']
+        avg_home_goals_league = league_averages[league]['avg_home_goals']
+        avg_away_goals_league = league_averages[league]['avg_away_goals']
+
+        # Calculate attack and defence strengths
+        home_attack_strength = home_team_data['avxG'] / avg_goals_league
+        away_defence_strength = away_team_data['avxG_conceded'] / avg_goals_league
+        away_attack_strength = away_team_data['avxG'] / avg_goals_league
+        home_defence_strength = home_team_data['avxG_conceded'] / avg_goals_league
+
+        # Calculate expected goals
+        xGHome = home_attack_strength * away_defence_strength * avg_home_goals_league
+        xGAway = away_attack_strength * home_defence_strength * avg_away_goals_league
+
+        # Calculate odds
+        odds_xG = calculate_outcomes(xGHome, xGAway)
+
+        # Update match document with calculated odds and intermediary variables
+        match_collection.update_one(
+            {'_id': match['_id']},
+            {'$set': {
+                'odds_xG': odds_xG,
+                'odds_xG_intermediaries': {
+                    'total_matches': league_averages[league]['total_matches'],
+                    'total_goals': league_averages[league]['total_goals'],
+                    'total_home_goals': league_averages[league]['total_home_goals'],
+                    'total_away_goals': league_averages[league]['total_away_goals'],
+                    'avg_goals_league': avg_goals_league,
+                    'avg_home_goals_league': avg_home_goals_league,
+                    'avg_away_goals_league': avg_away_goals_league,
+
+                    'home_xg': home_team_data['xG'],
+                    'home_av_xg': home_team_data['avxG'],
+                    'home_xg_conceded': home_team_data['xG_conceded'],
+                    'home_av_xg_conceded': home_team_data['avxG_conceded'],
+                    'home_goals': home_team_data['scored'],
+                    'home_conceded': home_team_data['missed'],
+                    'home_attack_strength': home_attack_strength,
+                    'home_defence_strength': home_defence_strength,
+
+                    'away_xg': away_team_data['xG'],
+                    'away_av_xg': away_team_data['avxG'],
+                    'away_xg_conceded': away_team_data['xG_conceded'],
+                    'away_av_xg_conceded': away_team_data['avxG_conceded'],
+                    'away_goals': away_team_data['scored'],
+                    'away_conceded': away_team_data['missed'],
+                    'away_attack_strength': away_attack_strength,
+                    'away_defence_strength': away_defence_strength,
+                    
+                    'xGHome': xGHome,
+                    'xGAway': xGAway
+                }
+            }}
+        )
+
+print("Match odds and intermediary variables updated.")
+
+exampleTeamData = {
+  "_id": {
+    "$oid": "657391867248d9d792fa41ed"
+  },
+  "id": "71",
+  "title": "Aston Villa",
+  "history": [
+    {
+      "h_a": "a",
+      "xG": 1.486,
+      "xGA": 4.32208,
+      "npxG": 1.486,
+      "npxGA": 4.32208,
+      "ppda": {
+        "att": 324,
+        "def": 28
+      },
+      "ppda_allowed": {
+        "att": 255,
+        "def": 17
+      },
+      "deep": 6,
+      "deep_allowed": 9,
+      "scored": 1,
+      "missed": 5,
+      "xpts": 0.1709,
+      "result": "l",
+      "date": "2023-08-12 16:30:00",
+      "wins": 0,
+      "draws": 0,
+      "loses": 1,
+      "pts": 0,
+      "npxGD": -2.83608
+    }
+  ],
+  "league": "EPL",
+  "away_record": {
+    "xG": 64.68272400000001,
+    "xGA": 94.784208,
+    "scored": 56,
+    "missed": 80,
+    "wins": 18,
+    "loses": 22,
+    "draws": 14,
+    "matches_played": 54,
+    "avxG": 1.1978282222222223,
+    "avxGA": 1.7552631111111112
+  },
+  "home_record": {
+    "xG": 96.20749199999999,
+    "xGA": 57.80858800000001,
+    "scored": 114,
+    "missed": 52,
+    "wins": 38,
+    "loses": 10,
+    "draws": 4,
+    "matches_played": 52,
+    "avxG": 1.8501440769230766,
+    "avxGA": 1.1117036153846156
+  },
+  "overall_record": {
+    "xG": 160.89021599999998,
+    "xGA": 152.59279599999994,
+    "scored": 170,
+    "missed": 132,
+    "wins": 56,
+    "loses": 32,
+    "draws": 18,
+    "matches_played": 106,
+    "avxG": 1.517832226415094,
+    "avxGA": 1.4395546792452825
+  }
+}
